1cbe766147788b2be98ef137ac11c34fb90ac89a
Semih OZER <63819478+SemihOzer@users.noreply.github.com>
2022-07-05 14:06:29 +0300

    Add files via upload
    random quesitons

---
+++ Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Angela Yu on 14/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let ballArray = ["ball1","ball2","ball3","ball4","ball5"];
+    
+    
+
+    @IBAction func buttonAction(_ sender: Any) {
+        imageView.alpha = 1;
+        imageView.image = UIImage(named:ballArray[Int.random(in: 0...4)]);
+    }
+    
+        
+}
+
